19518442a3956f96c8249bd0fdb79fcce334f3c9
chriskwong91 <chriskwong91@gmail.com>
2016-08-05 17:09:20 -0700

    Implement svg enemies, make enemies move and make a draggable player dot.

--- client/watchout.js
+++ client/watchout.js
@@ -1,18 +1,5 @@
 // start slingin' some d3 here.
 
-
-var test2 = d3.select('body').selectAll('div')
-	.data(['red', 'blue', 'green'])
-	.enter()
-	.append('span')
-	.style("background-color", function (d) {
-		return d; 
-	})
-	.attr("r", 0)
-	.transition().duration(1000)
-	.attr("r", 2.5);
-
-
 /*
 gameOptions =
   height: 450
@@ -25,152 +12,126 @@ gameOptions =
 
 /*	
 
-.attr("cx", function(d) { return d.x; })
-    .attr("cy", function(d) { return d.y; })
-    .attr("r", 2.5);
+.attr('cx', function(d) { return d.x; })
+    .attr('cy', function(d) { return d.y; })
+    .attr('r', 2.5);
 
 */
+//// CREATES GAME BOARD 
 
-
-/////////
-// Example to handle all three states. 
-/////////
-/*
-var circle = svg.selectAll("circle")
-  .data(data);
-
-circle.exit().remove();
-
-circle.enter().append("circle")
-    .attr("r", 2.5)
-  .merge(circle)
-    .attr("cx", function(d) { return d.x; })
-    .attr("cy", function(d) { return d.y; });
-*/
-
-////// Animation 
-/*
-circle.enter().append("circle")
-    .attr("r", 0)
-  .transition()
-    .attr("r", 2.5);
-*/
-
-
-var bodySelection = d3.select("body");
+var bodySelection = d3.select('body');
  
-var svgSelection = bodySelection.append("svg")
-       .attr("width", 500)
-       .attr("height", 500)
-       .style("border", "1px solid black");  
+var svgSelection = bodySelection.append('svg')
+       .attr('width', 500)
+       .attr('height', 500)
+       .style('border', '1px solid black');  
 
-/*
-var circleSelection = svgSelection.append("circle")
-       .attr("cx", 25)
-       .attr("cy", 25)
-       .attr("r", 0)
-       .style("fill", "blue")
-	   .transition().duration(1000)
-	   .attr("r", 25);
-*/
+///// PLACES ENEMIES ON THE BOARD
 
-var positionData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-var alphabetColor = function (num) {
-	var alphabet = 'abcdef';
-	alphabet = alphabet.split('');
-	var colorCombo = "#";
-	for (var i = 0; i < 3; i++) {
-		var index = Math.floor(alphabet.length * Math.random());
-		colorCombo += alphabet[index]; 
-	}
-	var num = Math.floor((Math.random() * 1000) - 10);
-	colorCombo += num; 
-	return colorCombo;
-}
-
-var circleSelection2 = svgSelection.selectAll("circle")
-	.data(positionData)
-	.enter()
-	.append("circle")
-	.attr("cx", function (d) { return d * 1.5 * 25; })
-    .attr("cy", function (d) { return d * 1.5 * 25; })
-    .attr("r", 0)
-    //.style("fill", function (d) { return alphabetColor() + (Math.floor((Math.random * 1000) - 10)) } )
-    .style("fill", function (d) { return alphabetColor(); } )
-    .transition().duration(500)
-    .attr("r", 20);
-    //.style("background-image", "url(nic-cage.png)" )
-/*
-var circleSelectionPlayer = d3.select("svg")
-	.data([1])
-	.enter()
-	.append("circlePlayer")
-	.attr("cx", function (d) { return 250; })
-    .attr("cy", function (d) { return 250; })
-    .attr("r", 0)
-    //.style("fill", function (d) { return alphabetColor() + (Math.floor((Math.random * 1000) - 10)) } )
-    .style("fill", function (d) { return alphabetColor(); } )
-    .transition().duration(500)
-    .attr("r", 20);
-    .text( 'P' );
-*/
-var randomPositionData = []
-
-function Coordinate(x, y) {
-	this.x = x;
-	this.y = y; 
-}
+var Coordinate = function (x, y) {
+  this.x = x;
+  this.y = y; 
+};
 
 var generatePositions = function (length) {
-	for (var i = 0; i < length; i++) {
-		var x = Math.floor(Math.random() * (500 - 50)); // 20 is to account for the radius. 
-		var y = Math.floor(Math.random() * (500 - 50)); // 20 is to account for the radius. 
-		randomPositionData[i] = new Coordinate(x,y);
-	}
-}
+  var randomPositionArray = [];
+  for (var i = 0; i < length; i++) {
+    var x = Math.max( (Math.random() * (500 - 50)), 50); // 20 is to account for the radius. 
+    var y = Math.max( (Math.random() * (500 - 50)), 50); // 20 is to account for the radius. 
+    randomPositionArray[i] = new Coordinate(x, y);
+  }
+  return randomPositionArray;
+};
 
-generatePositions(10); 
+var initialEnemyPosition = generatePositions(15);
 
-/*
-for (var j = 0; j < 10; j++) {
-	generatePositions();
-	circleMove
-}
-*/
+var imagePlacement = svgSelection.selectAll('image')
+  .data(initialEnemyPosition)
+  .enter()
+  .append('image')
+  .attr('x', function (d) { return d.x; })
+  .attr('y', function (d) { return d.y; })
+  .attr('xlink:href', 'shuriken.png')
+  .attr('height', 40)
+  .attr('width', 40);
 
-var move = function () {
-	var randomSpots = generatePositions(10); 
 
-    svgSelection.selectAll("circle")
-	.data(randomPositionData)
-	.attr("r", 20)
+///// PLACE PLAYER ON THE BOARD
+
+var drag = d3.behavior.drag()  
+  .on('dragstart', function() { player.style('fill', 'red'); })
+  .on('drag', function() { player.attr('cx', d3.event.x).attr('cy', d3.event.y); })
+  .on('dragend', function() { player.style('fill', 'black'); });
+
+
+var player = svgSelection.selectAll('circle')
+  .data([250])
+  .enter()
+  .append('circle')
+  .attr('cx', function (d) { return d; })
+  .attr('cy', function (d) { return d; })
+  //.attr('r', 0)
+  //.transition().duration(500)
+  .attr('r', 15)
+  .call(drag)
+  .style('fill', 'blue' ); 
+  
+  //.on('click', testFunction); 
+
+
+var testFunction = function () {
+  console.log('Player node has been clicked on!'); 
+};
+
+var testFunction2 = function () {
+  console.log('Player node has been dragged on!'); 
+};
+
+//var dragPlayer = d3.selectAll('circle').call(d3.behavior.drag().on('start', testFunction2)); 
+
+var playerMove = svgSelection.select('circle').on('click', function(event) {
+  this.call(d3.behavior.drag()); 
+  testFunction();
+  console.log(this);
+  console.log(d3.event);
+  var container = d3.selectAll('svg');  
+  //console.log(container);
+  // var mouseX = d3.mouse(d3.select('svg'))[0];
+  // var mouseY = d3.mouse(svgSelection)[1];
+  //console.log(d3.mouse(d3.selectAll('svg'))); 
+
+}); 
+
+//var playerDrag = svgSelection.select('circle').on('click'); 
+
+var dragged = function (d) {
+  d3.event.sourceEvent.stopPropagation();
+  d3.select(this)
+  .attr('cx', d.x = d3.event.x)
+  .attr('cy', d.y = d3.event.y);
+  console.log('We dragged something!?'); 
+};
+
+//var playerDrag = d3.behavior.drag().on('drag', testFunction2);
+//.on('start', testFunction2);
+
+
+
+//var playerDrag = svgSelection.select('circle').drag(); 
+
+
+var moveEnemies = function () {
+
+  var newEnemyPosition = generatePositions(15); 
+
+  svgSelection.selectAll('image')
+	.data(newEnemyPosition)
 	.transition().duration(500)
-	.style("fill", function (d) { return alphabetColor(); } )
-	.attr("cx", function (d) { return d.x })
-    .attr("cy", function (d) { return d.y });
+	.attr('x', function (d) { return d.x; })
+  .attr('y', function (d) { return d.y; });
 
-}
+};
 
-setInterval(move, 1500);
-
-/*
-generatePositions();
-
-var circleMove = svgSelection.selectAll("circle")
-	.data(randomPositionData)
-	.attr("r", 20)
-	.transition().duration(500)
-	.attr("cx", function (d) { return d })
-    .attr("cy", function (d) { return d });
+setInterval(moveEnemies, 1500);
 
 
-
-var circleMove = svgSelection.selectAll("circle")
-	.data(randomPositionData)
-	.attr("r", 20)
-	.transition().duration(500)
-	.attr("cx", function (d) { return d })
-    .attr("cy", function (d) { return d });
-
-*/
